d1cf784695d0368def48ea2a734ed8f4b9146a39
Jay Gao <gaoljie@gmail.com>
2020-01-01 22:26:56 -0800

    add component

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,40 @@
 import ts from "@wessberg/rollup-plugin-ts";
 import resolve from "@rollup/plugin-node-resolve";
+import glob from "glob";
 
-const config = {
-  input: "src/index.ts",
-  output: [
-    {
-      format: "cjs",
-      file: "dist/index.js"
-    }
-  ],
+const base = {
   external: ["react", "styled-components"],
   plugins: [ts(), resolve()]
 };
+
+const entries = glob
+  .sync("./src/components/**/index.tsx")
+  .map(item => {
+    return item.split("/")[3];
+  })
+  .map(componentName => {
+    return {
+      input: `src/components/${componentName}/index.tsx`,
+      output: {
+        file: `lib/${componentName}/index.js`,
+        format: "cjs",
+        sourcemap: true
+      },
+      ...base
+    };
+  });
+
+const config = [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        format: "cjs",
+        file: "lib/index.js"
+      }
+    ],
+    ...base
+  },
+  ...entries
+];
 export default config;
