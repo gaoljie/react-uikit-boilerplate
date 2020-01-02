03bbeacf7e9dd884accb21facfedd418f9b80b74
Jay Gao <gaoljie@gmail.com>
2020-01-02 08:43:33 -0800

    add theme

--- src/components/box/index.tsx
+++ src/components/box/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { size, SizeProp } from "../../types";
-import { theme } from "../theme";
+import theme from "../theme";
 
 function getPropWithSize(
   prop: SizeProp | string | number | undefined,
